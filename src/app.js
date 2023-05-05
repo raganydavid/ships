1e87c007d34c76c40c0416d83ebf7f06d23ee7ec
Ragány Dávid <david.ragany@gmail.com>
2023-05-05 22:50:42 +0200

    JS completed

--- src/app.js
+++ src/app.js
@@ -7,3 +7,41 @@ Date: 2023.05.05
 Github: 
 Licenc: GNU GPL 
 */
+
+const doc =  {
+    tbody: document.querySelector('#tbody')
+};
+
+const state = {
+    host: 'http://localhost:8000/',
+    ships: []
+};
+
+function getDatabase() {
+    let url = state.host +'ships';
+    fetch(url)
+    .then(response => response.json())
+    .then(result => {
+        state.ships = result;
+        render();
+        });
+}
+
+function render() {
+    let rows = '';
+    state.ships.forEach( (ship) => {
+        //console.log(ship.name);
+        rows += `
+        <tr>
+            <td>${ship.name}</td>
+            <td>${ship.length}</td>
+            <td>${ship.price}</td>
+            <td>${ship.person}</td>
+            <td>${ship.trailer}</td>
+        </tr>
+        `;
+    });
+    tbody.innerHTML = rows;
+}
+
+getDatabase();
